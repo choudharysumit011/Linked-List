002039579269dbf241b654a13ea4c745344336e7
choudharysumit011 <csumit408@gmail.com>
2024-03-02 09:59:45 +0530

    Linked list

---
+++ src/LinkedList.java
@@ -0,0 +1,82 @@
+public class LinkedList {
+// this is the main LinkedList class
+    private Node head;
+    private Node tail;
+    int size =0;
+    public LinkedList(){
+        this.size =0;
+    }
+
+//this is the Node object representing each individual element
+    private class Node{
+        private int value;// inserting data
+        private Node next;// pointer which is pointing to next
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node node) {
+            this.value = value;
+            this.next = node;
+        }
+    }
+
+    public void addFirst(int value){
+        Node node = new Node(value);// we will create Node
+       // node.value = value;
+        node.next = head;// first we will point the head to next pointer
+        head = node; // will then make head as first This effectively makes the newly created node the first node in the linked list, and now head points to it.
+
+        if(tail == null){
+            tail = head;
+        }
+        size++;
+
+    }
+
+    public void display(){
+        Node temp = head;
+        while (temp != null){
+            System.out.println(temp.value + "->");
+            temp = temp.next;
+        }
+       // temp = temp.next;
+
+    }
+
+    public void addLast(int value){
+        if(tail==null){
+            addFirst(value);
+
+        }
+        else {
+            Node node= new Node(value);
+            tail.next = node;
+            tail= node;
+        }
+    }
+
+    public void addatPos(int val, int index){
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+
+        if (index == 0) {
+            addFirst(val);
+        } else if (index == size) {
+            addLast(val);
+        } else {
+            Node temp = head;
+
+            for (int i = 1; i < index; i++) {
+                temp = temp.next;
+            }
+
+            Node node = new Node(val, temp.next);
+            temp.next = node;
+            size++;
+        }
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.addFirst(3);
+        list.addFirst(5);
+        list.addFirst(4);
+        list.addFirst(12);
+        list.addFirst(78);
+        list.addLast(55);
+        list.addatPos(190,1);
+        list.display();
+    }
+}
